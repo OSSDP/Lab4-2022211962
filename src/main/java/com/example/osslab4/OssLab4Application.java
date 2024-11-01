685e5c1f7e1f2172f8bce648ff2247b6e6b27661
Li-Xinrui-Siri <2825382552@qq.com>
2024-11-01 15:40:10 +0800

    步骤4：提交maven项目到步骤二中仓库

---
+++ src/main/java/com/example/osslab4/OssLab4Application.java
@@ -0,0 +1,13 @@
+package com.example.osslab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssLab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssLab4Application.class, args);
+    }
+
+}
